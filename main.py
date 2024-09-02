afc745a56d582c9bc131caf91b4369cae060974f
scrow-rgb <macrinho32@gmail.com>
2024-09-02 01:20:17 -0300

    refactor: funcionalidades do bot

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from discord.ext import commands
 import os
 import logging
 
-
-
+# Configuração de permissões (intents)
 permissoes = discord.Intents.default()
 permissoes.message_content = True
 permissoes.members = True
 permissoes.voice_states = True
+
 bot = commands.Bot(command_prefix='/', intents=permissoes)
 
-
+# Configuração de logging
 logging.basicConfig(level=logging.DEBUG)
 
-
+# Dicionário para mapear guildas (servidores) e canais padrão
+guild_channel_map = {}
 
 @bot.event
 async def on_ready():
@@ -22,14 +23,13 @@ async def on_ready():
     await bot.tree.sync()
     print(f'{bot.user} está online')
 
-# @bot.command()
-# async def sync(ctx:commands.Context):
-#     if ctx.author.id == 424351151534768128:
-#         server = discord.Object(id=1123842079169192016)
-#         sincs = await bot.tree.sync(guild=server)
-#         await ctx.reply(f'{len(sincs)} comandos sincronizados')
-#     else:
-#         await ctx.reply('Apenas o meu criador pode usar esse comando')
+    # Inicializa o mapeamento de guildas para canais padrão
+    for guild in bot.guilds:
+        default_channel = guild.text_channels[0] if guild.text_channels else None  # Seleciona o primeiro canal de texto, se disponível
+        if default_channel:
+            guild_channel_map[guild.id] = default_channel.id
+    print("Mapa de Guildas e Canais carregado:", guild_channel_map)
+
 
 @bot.event
 async def on_command_error(ctx: commands.Context, error):
@@ -41,19 +41,15 @@ async def on_command_error(ctx: commands.Context, error):
         except discord.HTTPException as e:
             print(f'Erro ao tentar apagar a mensagem: {e}')
     
-    await ctx.send('Desculpe, esse comando não existe. Tente `/ajuda`, para ver a minha lista de comandos', delete_after=5)
-    
+    await ctx.send('Desculpe, esse comando não existe. Tente `/ajuda` para ver a minha lista de comandos', delete_after=5)
 
 async def carregar_cogs():
-        
     for arquivo in os.listdir('cogs'):
         if arquivo.endswith('.py'):
-            await bot.load_extension(f'cogs.{arquivo[:-3]}')
+            try:
+                await bot.load_extension(f'cogs.{arquivo[:-3]}')
+            except Exception as e:
+                print(f'Falha ao carregar cog {arquivo}: {e}')
 
-
+# Lembre-se de nunca expor seu token publicamente!
 bot.run('MTI3ODI0MzUwNTQ1MzY2MjI0Mg.GG4Fp9.VH3wKbAc6HFSRTySUuiaKmHUclV3cCPvk7bx4s')
-
-
-
-
-    
